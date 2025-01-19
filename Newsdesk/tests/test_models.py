3a55baf6a805a806a78e3113021430e5e98f01a2
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-01-19 15:46:49 +0200

    test: create tests module
    Transfer and rename tests.py -> tests/test_models.py. Create class TestModels with tests for future implementation.

---
+++ Newsdesk/tests/test_models.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+
+class TestModels(TestCase):
+
+    def test_topic_str(self):
+        pass
+
+    def test_redactor_str(self):
+        pass
+
+    def test_newspaper_str(self):
+        pass
+
+    def test_redactor_with_experience(self):
+        pass
