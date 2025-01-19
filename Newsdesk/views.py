2164e608b685f5e0512013fb497c44893c3635c7
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-01-19 22:34:04 +0200

    add & implement context in index.html

--- Newsdesk/views.py
+++ Newsdesk/views.py
@@ -1,4 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
+from Newsdesk.models import Newspaper, Topic
+
+
 def index(request):
-    return render(request, "newsdesk/index.html")
+    context = {
+        "count_redactors": get_user_model().objects.count(),
+        "count_newspapers": Newspaper.objects.count(),
+        "count_topics": Topic.objects.count(),
+    }
+    return render(request, "newsdesk/index.html", context=context)
