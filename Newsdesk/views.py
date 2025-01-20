4e2735104922ca0fc6bde9e3d737b4940ef346ae
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-01-20 03:13:47 +0200

    make views login required

--- Newsdesk/views.py
+++ Newsdesk/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import generic
 
 from Newsdesk.models import Newspaper, Topic, Redactor
 
 
+@login_required
 def index(request):
     context = {
         "count_redactors": get_user_model().objects.count(),
@@ -14,26 +17,26 @@ def index(request):
     return render(request, "newsdesk/index.html", context=context)
 
 
-class TopicListView(generic.ListView):
+class TopicListView(LoginRequiredMixin, generic.ListView):
     model = Topic
     paginate_by = 10
 
 
-class NewspaperListView(generic.ListView):
+class NewspaperListView(LoginRequiredMixin, generic.ListView):
     model = Newspaper
     paginate_by = 5
 
 
-class NewspaperDetailView(generic.DetailView):
+class NewspaperDetailView(LoginRequiredMixin, generic.DetailView):
     model = Newspaper
     queryset = Newspaper.objects.all().select_related("topic")
     queryset = queryset.prefetch_related("publishers")
 
 
-class RedactorListView(generic.ListView):
+class RedactorListView(LoginRequiredMixin, generic.ListView):
     model = Redactor
     paginate_by = 10
 
 
-class RedactorDetailView(generic.DetailView):
+class RedactorDetailView(LoginRequiredMixin, generic.DetailView):
     model = Redactor
