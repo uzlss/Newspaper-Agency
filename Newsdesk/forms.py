681badac67cbb2b66afdbff280063eae8803a999
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-01-20 06:15:20 +0200

    create user creation form
    Implement RedactorCreationForm according to tests

---
+++ Newsdesk/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+from Newsdesk.models import Newspaper
+
+
+class RedactorCreationForm(UserCreationForm):
+    newspapers = forms.ModelMultipleChoiceField(
+        queryset=Newspaper.objects,
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = UserCreationForm.Meta.fields + ("years_of_experience", "newspapers")
