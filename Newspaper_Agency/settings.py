f151e46478285da5a433ee1b380b32ba2bef8b15
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-01-23 03:39:13 +0200

    add ASSETS_ROOT

--- Newspaper_Agency/settings.py
+++ Newspaper_Agency/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "Newsdesk.context_processors.cfg_assets_root"
             ],
         },
     },
@@ -124,6 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+ASSETS_ROOT = "/static/assets"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
