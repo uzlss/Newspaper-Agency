bd7d219abc12eb1983fc3ea7225720050692f3b8
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-01-23 14:48:37 +0200

    copy auth logic
    Copy authentication forms, urls, views -> Newsdesk.authentication

---
+++ Newsdesk/authentication/forms.py
@@ -0,0 +1,61 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Username",
+                "class": "form-control"
+            }
+        ))
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password",
+                "class": "form-control"
+            }
+        ))
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Username",
+                "class": "form-control"
+            }
+        ))
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "placeholder": "Email",
+                "class": "form-control"
+            }
+        ))
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password",
+                "class": "form-control"
+            }
+        ))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password check",
+                "class": "form-control"
+            }
+        ))
+    years_of_experience = forms.IntegerField(
+        widget=forms.NumberInput(
+            attrs={
+                "placeholder": "Years of experience",
+                "class": "form-control"
+            }
+        )
+    )
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email', 'password1', 'password2', 'years_of_experience')
--- Newspaper_Agency/urls.py
+++ Newspaper_Agency/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
        path('admin/', admin.site.urls),
        path("", include("Newsdesk.urls", namespace="newsdesk")),
-       path("accounts/", include("django.contrib.auth.urls")),
+       path("accounts/", include("Newsdesk.authentication.urls", )),
 ]
